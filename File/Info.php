8d5652c4feda566c86a6d89d7d2da7d9c1c3b7a8
Vitaly Doroshko <vdoroshko@mail.ru>
2015-06-21 11:24:28 +0300

    Comments refactored.

--- File/Info.php
+++ File/Info.php
@@ -197,7 +197,7 @@ class File_Info
      * Constructs a new File_Info object and obtains information about a file
      * on the specified path
      *
-     * @param  string  $path The file to obtain information about
+     * @param  string  $path The path to the file to obtain information about
      * @param  boolean $useIncludePath (optional) Whether to search for the file in the include path too
      * @throws File_InvalidPathException
      * @throws File_NotFoundException
