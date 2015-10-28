f940aaace55203b3d1c1da754e7e2536260246f2
Vitaly Doroshko <vdoroshko@mail.ru>
2015-10-28 15:23:17 +0300

    setFirstDayOfWeek() refactored.

--- Date/Calendar.php
+++ Date/Calendar.php
@@ -244,9 +244,9 @@ class Date_Calendar
         $this->_firstDayOfWeek = (integer)$firstDayOfWeek;
 
         $this->_firstDayOfMonthTimestamp = mktime(0, 0, 0, $this->_month, 1, $this->_year);
-        $firstSundayOfMonthTimestamp = strtotime(sprintf('-%d days', date('w', $this->_firstDayOfMonthTimestamp)), $this->_firstDayOfMonthTimestamp);
+        $firstSundayTimestamp = strtotime(sprintf('-%d days', date('w', $this->_firstDayOfMonthTimestamp)), $this->_firstDayOfMonthTimestamp);
 
-        $this->_firstDayTimestamp = strtotime(sprintf('+%d days', $firstDayOfWeek), $firstSundayOfMonthTimestamp);
+        $this->_firstDayTimestamp = strtotime(sprintf('+%d days', $firstDayOfWeek), $firstSundayTimestamp);
         if ($this->_firstDayTimestamp > $this->_firstDayOfMonthTimestamp) {
             $this->_firstDayTimestamp = strtotime('-7 days', $this->_firstDayTimestamp);
         }
